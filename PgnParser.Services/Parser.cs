dffdcab6d62657b0ccd70b612b4c46e572e58215
Ace Baum <jaybaum.03@gmail.com>
2025-07-16 19:19:43 -0400

    refactor to move file related services into Console application

---
+++ PgnParser.Services/Parser.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using PgnParser.DataModels.Interfaces;
+using PgnParser.DataModels.Models;
+
+namespace PgnParser.Services
+{
+    public class Parser
+    {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Parser(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Handles parsing process from PGN input file to generated
+        /// PGN output files.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public List<IGame> Parse(List<string> input)
+        {
+            var games = new List<IGame>();
+
+            var i = 0;
+
+            while (i < input.Count)
+            {
+                // build tags
+                var tags = new StringBuilder();
+                while (i < input.Count && input[i].StartsWith("["))
+                {
+                    tags.Append($"{Environment.NewLine}{input[i]}");
+                    i++;
+                }
+                
+                // build moves
+                var moves = new StringBuilder();
+                while (i < input.Count && !input[i].StartsWith("["))
+                {
+                    moves.Append(input[i]);
+                    i++;
+                }
+                
+                games.Add(new Game(tags.ToString(), moves.ToString()));
+
+                i++;
+            }
+            
+            return games;
+        }
+    }
+}
